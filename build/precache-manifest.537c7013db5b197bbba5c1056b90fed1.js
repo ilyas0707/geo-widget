c87eae7671ebac706d49767b2baa22dc11a24bbf
ilyas0707 <ilyas.yangurazov.iy@gmail.com>
2020-06-23 14:38:19 +0600

    initial commit

---
+++ build/precache-manifest.537c7013db5b197bbba5c1056b90fed1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c07001abff040540281e702cccebf4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07b6f39343a24e80d68b",
+    "url": "/static/css/2.98167188.chunk.css"
+  },
+  {
+    "revision": "95084982349a3e8626ab",
+    "url": "/static/css/main.abf000d7.chunk.css"
+  },
+  {
+    "revision": "07b6f39343a24e80d68b",
+    "url": "/static/js/2.67670609.chunk.js"
+  },
+  {
+    "revision": "6791e7fb9584d65ed87d655035c7da95",
+    "url": "/static/js/2.67670609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95084982349a3e8626ab",
+    "url": "/static/js/main.adb79150.chunk.js"
+  },
+  {
+    "revision": "080b4fd49f8f9df8547b",
+    "url": "/static/js/runtime-main.8dc5e5df.js"
+  }
+]);
